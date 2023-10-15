eda8843207b7136ba7d62683fdc2336e045c07a6
Priya Kushwaha <99608708+Kushpriya@users.noreply.github.com>
2023-10-15 19:38:01 +0545

    space shooter game

---
+++ Space shooter/background.js
@@ -0,0 +1,46 @@
+class Background {
+    constructor(x = 0, y = 0) {
+      this.position = {
+        x: x,
+        y: y,
+      };
+  
+      this.size = {
+        width: canvas.width,
+        height: canvas.height,
+      };
+  
+      this.velocity = {
+        x: 0,
+        y: 0,
+      };
+  
+      this.img = new Image();
+      this.img.src = "./background.png";
+    }
+  
+    draw() {
+      c.drawImage(
+        this.img,
+        this.position.x,
+        this.position.y,
+        this.size.width,
+        this.size.height
+      );
+    }
+  
+    moveLeft() {
+      if (this.position.x + this.size.width <= 0) {
+        this.position.x = this.size.width;
+      }
+      this.position.x += this.velocity.x;
+    }
+  
+    update(isDead) {
+      this.draw();
+      if (!isDead) {
+        this.moveLeft();
+      }
+    }
+  }
+  
